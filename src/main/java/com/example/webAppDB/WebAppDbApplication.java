9a932062a06b2c57fb61a186d0798a4e815931de
George <indranil.ariunbold@icloud.com>
2018-04-11 16:42:27 +0200

    v1

---
+++ src/main/java/com/example/webAppDB/WebAppDbApplication.java
@@ -0,0 +1,12 @@
+package com.example.webAppDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppDbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/webAppDB/WebAppDbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.webAppDB;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebAppDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
