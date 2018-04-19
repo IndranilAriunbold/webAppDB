d8888e833276f0b2e6931a5ca9bc47b8db7b0c0e
George <indranil.ariunbold@icloud.com>
2018-04-19 05:52:51 +0200

    dockerfile

--- src/main/java/com/example/webAppDB/UserRepository.java
+++ src/main/java/com/example/webAppDB/UserRepository.java
@@ -3,7 +3,7 @@ package com.example.webAppDB;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-import hello.User;
+import com.example.webAppDB.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
